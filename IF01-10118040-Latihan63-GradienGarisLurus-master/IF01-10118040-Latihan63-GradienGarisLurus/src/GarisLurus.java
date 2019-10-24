23ed370ed457566da9bf7594aaadd0034e8bab8c
Moch Yudha Rusdian <55824602+Yudharus@users.noreply.github.com>
2019-10-24 14:50:01 +0700

    Add files via upload

---
+++ IF01-10118040-Latihan63-GradienGarisLurus-master/IF01-10118040-Latihan63-GradienGarisLurus/src/GarisLurus.java
@@ -0,0 +1,15 @@
+/*
+ * NAMA              : Moch Yudha Rusdian
+ * KELAS             : IF-01
+ * NIM               : 10118040
+ * DESKRIPSI PROGRAM : Program ini berisi program untuk mencari gradien dari beberapa titik  
+ */
+
+/**
+ *
+ * @author Fatahillah Seno
+ */
+public interface GarisLurus {
+
+    public int hitungGradien();
+}
